ce5ee4ce27cce404f564ea896917425f64e0fcc0
qaws7791 <qaws7791@gmail.com>
2025-05-13 07:47:00 +0900

    feat: hono 컨텍스트 타입 정의 추가

---
+++ apps/api/src/types/hono.types.ts
@@ -0,0 +1,20 @@
+import type { z } from "@hono/zod-openapi";
+import { Env } from "hono";
+
+export type ZodSchema = z.AnyZodObject | z.ZodArray<z.AnyZodObject>;
+
+export type Context = Env & {
+  Variables: {
+    user?: {
+      id: number;
+      name: string;
+      email: string | null;
+      emailVerified: Date | null;
+      profileImageUrl: string | null;
+      role: "user" | "creator";
+      status: string;
+      createdAt: Date;
+      updatedAt: Date;
+    } | null;
+  };
+};
