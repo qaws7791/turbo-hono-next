a31430af63697ab53f88f5e265d87bbfaac10e23
qaws7791 <qaws7791@gmail.com>
2025-05-13 07:48:57 +0900

    feat: hono 서버 설정 및 Swagger UI 통합, CORS 및 CSRF 미들웨어 추가

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,56 @@
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono } from "@hono/zod-openapi";
+import "dotenv/config";
+import { cors } from "hono/cors";
+import { csrf } from "hono/csrf";
+import { logger } from "hono/logger";
+import platformRoutes from "./routes/platform";
+import { Context } from "./types/hono.types";
+const ORIGIN: string[] | string = "http://localhost:3000";
 
-const app = new Hono()
+const app = new OpenAPIHono<Context>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(logger());
+app.use(
+  "*",
+  cors({
+    origin: ORIGIN,
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    exposeHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+app.use(
+  csrf({
+    origin: ORIGIN,
+  }),
+);
 
-export default app
+app.get("/ui", swaggerUI({ url: "/doc" }));
+app.get("/", (c) => {
+  return c.json({
+    message: "Hello World",
+  });
+});
+
+app.route("/platform", platformRoutes);
+
+app.doc("/doc", {
+  openapi: "3.1.0",
+  info: {
+    title: "API",
+    version: "1.0.0",
+    description: "API",
+  },
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
