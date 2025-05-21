81065fd8e101f6cf3dd10364fa244f9cea497746
qaws7791 <qaws7791@gmail.com>
2025-05-21 16:27:34 +0900

    refactor: symbols를 사용하여 의존성 주입을 위한 컨테이너 설정 개선

--- apps/api/src/containers/index.ts
+++ apps/api/src/containers/index.ts
@@ -1,3 +1,4 @@
+import { DI_SYMBOLS } from "@/containers/symbols";
 import { initializeDatabase } from "@/db";
 import { AccountRepository } from "@/db/repositories/account.repository";
 import { EmailVerificationTokenRepository } from "@/db/repositories/email-verification-token.repository";
@@ -14,27 +15,38 @@ import { ReactionService } from "@/services/reaction.service";
 import { StoryService } from "@/services/story.service";
 import { UserService } from "@/services/user.service";
 import { Container } from "inversify";
+
 export const container = new Container();
 const db = initializeDatabase();
 
 // DB
-container.bind<DbClient>("db").toDynamicValue(() => db);
+container.bind<DbClient>(DI_SYMBOLS.db).toDynamicValue(() => db);
 
 // Repositories
 container
-  .bind<EmailVerificationTokenRepository>("emailVerificationTokenRepository")
+  .bind<EmailVerificationTokenRepository>(
+    DI_SYMBOLS.emailVerificationTokenRepository,
+  )
   .to(EmailVerificationTokenRepository);
-container.bind<UserRepository>("userRepository").to(UserRepository);
-container.bind<AccountRepository>("accountRepository").to(AccountRepository);
-container.bind<SessionRepository>("sessionRepository").to(SessionRepository);
-container.bind<ObjectRepository>("objectRepository").to(ObjectRepository);
-container.bind<StoryRepository>("storyRepository").to(StoryRepository);
-container.bind<ReactionRepository>("reactionRepository").to(ReactionRepository);
+container.bind<UserRepository>(DI_SYMBOLS.userRepository).to(UserRepository);
+container
+  .bind<AccountRepository>(DI_SYMBOLS.accountRepository)
+  .to(AccountRepository);
+container
+  .bind<SessionRepository>(DI_SYMBOLS.sessionRepository)
+  .to(SessionRepository);
+container
+  .bind<ObjectRepository>(DI_SYMBOLS.objectRepository)
+  .to(ObjectRepository);
+container.bind<StoryRepository>(DI_SYMBOLS.storyRepository).to(StoryRepository);
+container
+  .bind<ReactionRepository>(DI_SYMBOLS.reactionRepository)
+  .to(ReactionRepository);
 
 // Services
-container.bind<AuthService>("authService").to(AuthService);
-container.bind<ObjectService>("objectService").to(ObjectService);
-container.bind<R2Service>("r2Service").to(R2Service);
-container.bind<StoryService>("storyService").to(StoryService);
-container.bind<ReactionService>("reactionService").to(ReactionService);
-container.bind<UserService>("userService").to(UserService);
+container.bind<AuthService>(DI_SYMBOLS.authService).to(AuthService);
+container.bind<ObjectService>(DI_SYMBOLS.objectService).to(ObjectService);
+container.bind<R2Service>(DI_SYMBOLS.r2Service).to(R2Service);
+container.bind<StoryService>(DI_SYMBOLS.storyService).to(StoryService);
+container.bind<ReactionService>(DI_SYMBOLS.reactionService).to(ReactionService);
+container.bind<UserService>(DI_SYMBOLS.userService).to(UserService);
---
+++ apps/api/src/containers/symbols.ts
@@ -0,0 +1,18 @@
+export const DI_SYMBOLS = {
+  db: Symbol("db"),
+  // Repositories
+  emailVerificationTokenRepository: Symbol("emailVerificationTokenRepository"),
+  userRepository: Symbol("userRepository"),
+  accountRepository: Symbol("accountRepository"),
+  sessionRepository: Symbol("sessionRepository"),
+  objectRepository: Symbol("objectRepository"),
+  storyRepository: Symbol("storyRepository"),
+  reactionRepository: Symbol("reactionRepository"),
+  // Services
+  authService: Symbol("authService"),
+  objectService: Symbol("objectService"),
+  r2Service: Symbol("r2Service"),
+  storyService: Symbol("storyService"),
+  reactionService: Symbol("reactionService"),
+  userService: Symbol("userService"),
+};
