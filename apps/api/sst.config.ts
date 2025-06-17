688349c0517c61e4870895455bf7f241ce69ed25
qaws7791 <qaws7791@gmail.com>
2025-06-18 03:51:45 +0900

    feat: sst 리전 설정

--- apps/api/sst.config.ts
+++ apps/api/sst.config.ts
@@ -8,6 +8,11 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
+      providers: {
+        aws:{
+          region: "ap-northeast-2"
+        }
+      }
     };
   },
   async run() {
