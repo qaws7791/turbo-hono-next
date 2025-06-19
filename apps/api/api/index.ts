35ab5faa712782d4aeeeb3b9c6df3a1254370f19
qaws7791 <qaws7791@gmail.com>
2025-06-19 21:06:14 +0900

    feat: tsconfig 임시 교체

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,7 +1,6 @@
 import { handle } from "@hono/node-server/vercel";
 
-// eslint-disable-next-line
-// @ts-expect-error
-import app from "../dist/src/app.cjs";
+
+import app from "../dist/src/app.js";
 
 export default handle(app);
