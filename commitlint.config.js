ca38b963abc457d88fa0f359f934c9bcb8effd07
qaws7791 <qaws7791@gmail.com>
2025-09-17 22:14:50 +0900

    feat: commitlint 설정 확장 및 커스텀 규칙 추가
    - type-case 규칙 비활성화
    - 허용되는 커밋 타입에 remove 추가
    - 설정 파일 구조화로 가독성 개선

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,24 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-case": [0],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "remove",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
+};
