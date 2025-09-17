86ccfa492d2c600782a90f1825754f83a743731d
qaws7791 <qaws7791@gmail.com>
2025-09-17 22:09:11 +0900

    remove: api 앱 제거

--- apps/api/src/shared/errors/base.error.ts
+++
@@ -1,9 +0,0 @@
-export class BaseError extends Error {
-  constructor(
-    public readonly message: string,
-    public readonly statusCode: number,
-    public readonly name: string,
-  ) {
-    super(message);
-  }
-}
