8915a3fec3b29a1ca7bba2e09e6524b0998113a5
qaws7791 <qaws7791@gmail.com>
2025-09-23 23:35:29 +0900

    feat: utils 내보내기

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -1,9 +1,12 @@
+import { twMerge } from "tailwind-merge";
 import { tv } from "tailwind-variants";
 
 export const focusRing = tv({
-  base: "data-[focused]:outline-none data-[focused]:ring-2 data-[focused]:ring-ring data-[focused]:ring-offset-2 data-[focus-visible]:outline-none"
+  base: "data-[focused]:outline-none data-[focused]:ring-2 data-[focused]:ring-ring data-[focused]:ring-offset-2 data-[focus-visible]:outline-none",
 });
 
 export const focusVisibleRing = tv({
-  base: "data-[focus-visible]:outline-none data-[focus-visible]:ring-2 data-[focus-visible]:ring-ring data-[focus-visible]:ring-offset-2"
+  base: "data-[focus-visible]:outline-none data-[focus-visible]:ring-2 data-[focus-visible]:ring-ring data-[focus-visible]:ring-offset-2",
 });
+
+export { twMerge };
