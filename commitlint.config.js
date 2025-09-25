581bf20e12ba820d557b73d2b7801425497e5ce2
qaws7791 <qaws7791@gmail.com>
2025-09-25 21:05:29 +0900

    chore: commitlint 규칙 개선
      - subject-case 제한 해제하여 대문자 시작 허용
      - design 커밋 타입 추가

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "type-case": [0],
+    "subject-case": [0], // 제목 케이스 제한 해제 (대문자 시작 허용)
     "type-enum": [
       2,
       "always",
@@ -9,6 +10,7 @@ module.exports = {
         "build",
         "chore",
         "ci",
+        "design", // design 타입 추가
         "docs",
         "feat",
         "fix",
